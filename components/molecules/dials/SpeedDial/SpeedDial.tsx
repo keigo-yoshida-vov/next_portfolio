07e23015160204b1526833e47eeade2e8dff10fd
keigo-yoshida-vov <yoshidak51214@gmail.com>
2021-11-01 19:40:32 +0900

    speeddial

---
+++ components/molecules/dials/SpeedDial/SpeedDial.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import FormControl from '@mui/material/FormControl'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormLabel from '@mui/material/FormLabel'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+import Switch from '@mui/material/Switch'
+import SpeedDial, { SpeedDialProps } from '@mui/material/SpeedDial'
+import SpeedDialIcon from '@mui/material/SpeedDialIcon'
+import SpeedDialAction from '@mui/material/SpeedDialAction'
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined'
+import SaveIcon from '@mui/icons-material/Save'
+import PrintIcon from '@mui/icons-material/Print'
+import ShareIcon from '@mui/icons-material/Share'
+
+const StyledSpeedDial = styled(SpeedDial)(({ theme }) => ({
+    position: 'absolute',
+    '&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight': {
+        top: theme.spacing(2),
+        right: theme.spacing(2),
+    },
+}))
+
+const actions = [
+    { icon: <FileCopyIcon />, name: 'Copy' },
+    { icon: <SaveIcon />, name: 'Save' },
+    { icon: <PrintIcon />, name: 'Print' },
+    { icon: <ShareIcon />, name: 'Share' },
+]
+
+export const CustomSpeedDial = () => {
+    const [direction, setDirection] =
+        React.useState<SpeedDialProps['direction']>('down')
+
+    return (
+        <Box sx={{ transform: 'translateZ(0px)', flexGrow: 1 }}>
+            <Box sx={{ position: 'relative', mt: 0, height: 320 }}>
+                <StyledSpeedDial
+                    ariaLabel="SpeedDial playground example"
+                    icon={<SpeedDialIcon />}
+                    direction={direction}
+                >
+                    {actions.map((action) => (
+                        <SpeedDialAction
+                            key={action.name}
+                            icon={action.icon}
+                            tooltipOpen
+                            tooltipTitle={action.name}
+                        />
+                    ))}
+                </StyledSpeedDial>
+            </Box>
+        </Box>
+    )
+}
--- components/templates/Home/HomeTemplate.tsx
+++ components/templates/Home/HomeTemplate.tsx
@@ -1,23 +1,12 @@
-import React, { memo, useState, useCallback } from 'react'
+import { CustomSpeedDial } from '@/components/molecules/dials/SpeedDial/SpeedDial'
+import React, { memo } from 'react'
 import scss from './home-template.module.scss'
-import { BaseHeader } from '@/components/organisms/header/BaseHeader/BaseHeader'
-import { ActionAreaCard } from '@/components/atoms/Cards/ActionAreaCard/ActionAreaCard'
 
 export const HomeTemplate = memo(() => {
-    const onClickCard = () => {
-        alert('click')
-    }
     return (
         <>
             <main className={`${scss.main}`}>
-                <BaseHeader />
-                <ActionAreaCard image='cat.jpg' color='lightgray' size={300} onClick={onClickCard}>
-                    <div>
-                        <p>title</p>
-                        <p>contents</p>
-                        <p>contents</p>
-                    </div>
-                </ActionAreaCard>
+                <CustomSpeedDial />
             </main>
         </>
     )
